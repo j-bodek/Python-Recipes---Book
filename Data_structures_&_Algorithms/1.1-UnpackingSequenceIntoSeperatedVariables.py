69ce13da773b8b634e77e8a023fc5ac65959d87d
LilJack118 <78555188+LilJack118@users.noreply.github.com>
2021-12-28 16:08:23 +0100

    'ds

---
+++ Data_structures_&_Algorithms/1.1-UnpackingSequenceIntoSeperatedVariables.py
@@ -0,0 +1,23 @@
+
+
+
+data = ['ACM', 50, 91.1, (2012, 12, 21)]
+name, shares, price, date = data
+print(name)
+print(shares)
+print(price)
+print(date)
+
+# ACM
+# 50
+# 91.1
+# (2012, 12, 21)
+
+# this technique works for all iterable objects
+
+# pass some variables 
+data = ['ACM', 50, 91.1, (2012, 12, 21)]
+_, shares, price, _ = data
+
+
+
