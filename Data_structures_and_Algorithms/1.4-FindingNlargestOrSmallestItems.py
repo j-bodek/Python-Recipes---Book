80c57b85a6fafe8ff63fdb34a1e4268ccf4202d1
LilJack118 <78555188+LilJack118@users.noreply.github.com>
2021-12-28 16:49:50 +0100

    '1.4'

---
+++ Data_structures_and_Algorithms/1.4-FindingNlargestOrSmallestItems.py
@@ -0,0 +1,27 @@
+
+
+
+import heapq
+
+nums = [1,6,2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(heapq.nlargest(3,nums))
+print(heapq.nsmallest(3,nums))
+
+
+# with keys
+
+portfolio = [
+ {'name': 'IBM', 'shares': 100, 'price': 91.1},
+ {'name': 'AAPL', 'shares': 50, 'price': 543.22},
+ {'name': 'FB', 'shares': 200, 'price': 21.09},
+ {'name': 'HPQ', 'shares': 35, 'price': 31.75},
+ {'name': 'YHOO', 'shares': 45, 'price': 16.35},
+ {'name': 'ACME', 'shares': 75, 'price': 115.65}
+]
+cheap = heapq.nsmallest(2, portfolio, key=lambda s: s['price'])
+expensive = heapq.nlargest(2, portfolio, key=lambda s: s['price'])
+print(cheap)
+print(expensive)
+
+
+
